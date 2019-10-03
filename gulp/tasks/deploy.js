833a37bef9c19f7a778d132c357b776b754eab9f
Shawn Catanzarite <scatanzarite@sharespost.com>
2019-10-03 16:54:27 -0700

    Update 2019-10-03T23:54:22.468Z

--- gulp/tasks/deploy.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var runSequence = require('run-sequence')
-// Necessary Plugins
-var gulp   = require('gulp');
-module.exports = gulp.task('deploy', function(){
-  runSequence('js', 'jade', 'stylus', 'imagemin', 'deploy-pages');
-});
